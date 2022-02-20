30d7dc08755d81f931b6fb2f12f54df50feaf00d
Hikari <954083089@qq.com>
2022-02-21 00:10:56 +0800

    re:登录封装

--- src/globalData.js
+++ src/globalData.js
@@ -1,5 +1,15 @@
 
-let userInfo = null
+const userInfo = () => {
+  let info
+  wx.getStorage({
+    key: 'userInfo',
+    success: res => {
+      console.log(res)
+      info = res
+    }
+  })
+  return info
+}
 
 export default {
   userInfo
