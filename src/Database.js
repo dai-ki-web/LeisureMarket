b89c4523abb0bec614468183548103c73720e64c
Hikari <954083089@qq.com>
2022-02-22 17:54:09 +0800

    feat: 发布页表单验证

--- src/Database.js
+++ src/Database.js
@@ -10,3 +10,9 @@ export const dbSearchOpenid = (sheetName, data) => {
   const sheet = db.collection(sheetName)
   return sheet.where({ _openid: _.eq(data) }).get()
 }
+
+// 展示详情页时通过数据id向数据库查询数据
+export const getGoodsData = (id) => {
+  const sheet = db.collection('goods-shelf')
+  return sheet.doc(id).get()
+}
